0844f1d1a730aad7ff43e82f674dc2fd94d32504
ハムスケ ハム <daiki.esdfth12488@gmail.com>
2025-01-20 07:55:43 +0900

    game-play gui improvement part 1

---
+++ src/main/java/com/hamusuke/numguesser/client/gui/component/JXGameTablePanel.java
@@ -0,0 +1,13 @@
+package com.hamusuke.numguesser.client.gui.component;
+
+import org.jdesktop.swingx.JXPanel;
+
+import java.awt.*;
+
+public class JXGameTablePanel extends JXPanel {
+    @Override
+    public void paint(Graphics g) {
+
+        super.paint(g);
+    }
+}
