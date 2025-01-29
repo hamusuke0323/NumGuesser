8c953b547e83e25c6374d0cb7a26d789fc070719
ハムスケ ハム <daiki.esdfth12488@gmail.com>
2025-01-29 13:36:57 +0900

    update version

--- src/main/java/com/hamusuke/numguesser/Constants.java
+++ src/main/java/com/hamusuke/numguesser/Constants.java
@@ -1,7 +1,7 @@
 package com.hamusuke.numguesser;
 
 public class Constants {
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.10.0";
     public static final int PROTOCOL_VERSION = 32;
     public static final int CARD_WIDTH = 75;
     public static final int CARD_HEIGHT = 108;
